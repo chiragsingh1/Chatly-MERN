48e53c74d7b9164c54ec3917bfe5d75f0ab4b2b7
chiragsingh1 <imchirag.2717@gmail.com>
2022-03-15 16:33:47 +0530

    Remove and Add a user in a group

--- backend/routes/chatRoute.js
+++ backend/routes/chatRoute.js
@@ -6,6 +6,8 @@ const {
   fetchChats,
   createGroupChat,
   renameGroup,
+  removeFromGroup,
+  addToGroup,
 } = require("../controllers/chatController");
 
 const router = express.Router();
@@ -14,7 +16,7 @@ router.route("/").post(protect, accessChat);
 router.route("/").get(protect, fetchChats);
 router.route("/group").post(protect, createGroupChat);
 router.route("/rename").put(protect, renameGroup);
-// router.route("/groupremove").put(protect, removeFromGroup);
-// router.route("/groupadd").put(protect, addToGroup);
+router.route("/groupremove").put(protect, removeFromGroup);
+router.route("/groupadd").put(protect, addToGroup);
 
 module.exports = router;
