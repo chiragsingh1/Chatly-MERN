bb10fbe50dfcec5408048499509178f0575a241a
chiragsingh1 <imchirag.2717@gmail.com>
2022-03-15 16:26:45 +0530

    rename group API

--- backend/controllers/chatController.js
+++ backend/controllers/chatController.js
@@ -73,6 +73,8 @@ const fetchChats = asyncHandler(async (req, res) => {
   }
 });
 
+// Logic to create a group chat with the admin as logged in user
+// and group member sent in request
 const createGroupChat = asyncHandler(async (req, res) => {
   if (!req.body.users || !req.body.name) {
     return res.status(400).send({ message: "Please Fill all the feilds" });
@@ -107,4 +109,28 @@ const createGroupChat = asyncHandler(async (req, res) => {
   }
 });
 
-module.exports = { accessChat, fetchChats, createGroupChat };
+// Logic to rename a group chat
+const renameGroup = asyncHandler(async (req, res) => {
+  const { chatId, chatName } = req.body;
+
+  const updatedChat = await Chat.findByIdAndUpdate(
+    chatId,
+    {
+      chatName: chatName,
+    },
+    {
+      new: true,
+    }
+  )
+    .populate("users", "-password")
+    .populate("groupAdmin", "-password");
+
+  if (!updatedChat) {
+    res.status(404);
+    throw new Error("Chat Not Found");
+  } else {
+    res.json(updatedChat);
+  }
+});
+
+module.exports = { accessChat, fetchChats, createGroupChat, renameGroup };
