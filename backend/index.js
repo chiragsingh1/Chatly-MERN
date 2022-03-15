c30c583fbc2c80f3af3af92031ec1e2099b61354
chiragsingh1 <imchirag.2717@gmail.com>
2022-03-15 15:43:41 +0530

    create a chat API

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
-const userRoutes = require("./routes/userRoute");
+const userRoute = require("./routes/userRoute");
+const chatRoute = require("./routes/chatRoute");
+
 const { errorHandler, notFound } = require("./middleware/errorMiddleware");
 
 dotenv.config();
@@ -11,8 +13,10 @@ const app = express();
 
 app.use(express.json());
 
-app.use("/api/user", userRoutes);
-// app.use("/api/chat", chatRoutes);
+// Routes
+app.use("/api/user", userRoute);
+app.use("/api/chat", chatRoute);
+// Error Handling Routes
 app.use(notFound);
 app.use(errorHandler);
 
