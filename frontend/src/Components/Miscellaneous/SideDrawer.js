8feb2db9af920ca10cca8dd02db6d938313d9bbb
chiragsingh1 <imchirag.2717@gmail.com>
2022-03-16 13:41:52 +0530

    Added Context API, integrated Group Create API, built UI for the same

--- frontend/src/Components/Miscellaneous/SideDrawer.js
+++ frontend/src/Components/Miscellaneous/SideDrawer.js
@@ -47,8 +47,8 @@ function SideDrawer() {
   const {
     setSelectedChat,
     user,
-    notification,
-    setNotification,
+    // notification,
+    // setNotification,
     chats,
     setChats,
   } = ChatState();
@@ -95,7 +95,39 @@ function SideDrawer() {
     }
   };
 
-  const accessChat = async (userId) => {};
+  const accessChat = async (userId) => {
+    console.log(userId);
+
+    try {
+      setLoadingChat(true);
+
+      const config = {
+        headers: {
+          "Content-type": "application/json",
+          Authorization: `Bearer ${user.token}`,
+        },
+      };
+
+      const { data } = await axios.post(`/api/chat`, { userId }, config);
+
+      if (!chats.find((c) => c._id === data._id)) setChats([data, ...chats]);
+
+      setSelectedChat(data);
+
+      setLoadingChat(false);
+
+      onClose();
+    } catch (error) {
+      toast({
+        title: "Error fetching the chat",
+        description: error.message,
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom-left",
+      });
+    }
+  };
 
   return (
     <>
