62b132846aea4cc35898ed5a6e495477c3ec2f5f
chiragsingh1 <imchirag.2717@gmail.com>
2022-03-16 17:20:30 +0530

    send and fetch chats API

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const userRoute = require("./routes/userRoute");
 const chatRoute = require("./routes/chatRoute");
+const messageRoute = require("./routes/messageRoute");
 
 const { errorHandler, notFound } = require("./middleware/errorMiddleware");
 
@@ -16,6 +17,8 @@ app.use(express.json());
 // Routes
 app.use("/api/user", userRoute);
 app.use("/api/chat", chatRoute);
+app.use("/api/message", messageRoute);
+
 // Error Handling Routes
 app.use(notFound);
 app.use(errorHandler);
